51c2fe5bd139a448ed39409697e84f228405b3a5
Miklos Megyes <m.miklos05@gmail.com>
2021-03-24 23:57:50 +0100

    wip

--- react-app-actions/packages/runtime/dispatch.js
+++ react-app-actions/packages/runtime/dispatch.js
@@ -1,11 +1,18 @@
 import { computeAccessibleName } from 'dom-accessibility-api';
-import { roles as allRoles } from 'aria-query';
+import { roles as allRoles, elementRoles } from 'aria-query';
 
 export async function dispatch(renderer, command) {
-    console.log('roots', { roots, computeAccessibleName, allRoles });
+    console.log('roots', { roots, computeAccessibleName, allRoles, elementRoles: Array.from(elementRoles) });
     const roots = renderer.getFiberRoots();
     renderer.listFibersByPredicate(roots[0].current, fiber => {
-        console.log('visited:', renderer.getDisplayName(fiber));
+        // if (fiber.type && fiber.type.__REACT_APP_ACTIONS__) {
+        //     console.log('visited:', renderer.getDisplayName(fiber), fiber.type.__REACT_APP_ACTIONS__);
+        // }
+
+        if (fiber.stateNode instanceof HTMLElement) {
+            console.log('visited:', renderer.getDisplayName(fiber), computeAccessibleName(fiber.stateNode));
+        }
+
         return false;
     });
     debugger;
