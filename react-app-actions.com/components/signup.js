9ffb2baa76333d04b4f2d888006fed8b9c3b68f5
Miklos Megyes <m.miklos05@gmail.com>
2021-04-05 00:35:24 +0200

    convertkit

--- react-app-actions.com/components/signup.js
+++ react-app-actions.com/components/signup.js
@@ -1,6 +1,109 @@
+import { useState } from 'react';
+
+const STATUSES = {
+    INIT: 'INIT',
+    SUBMITTING: 'SUBMITTING',
+    ERROR: 'ERROR',
+    SUCCESS: 'SUCCESS',
+};
+
 function SignUp({ label = 'Join wait list' }) {
+    const [status, setStatus] = useState(STATUSES.INIT);
+    const onSubmit = event => {
+        event.preventDefault();
+
+        setStatus(STATUSES.SUBMITTING);
+
+        fetch('/api/subscribe', {
+            method: 'POST',
+            body: JSON.stringify({
+                email: event.target.elements.email.value,
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(() => {
+                setStatus(STATUSES.SUCCESS);
+            })
+            .catch(() => {
+                setStatus(STATUSES.ERROR);
+            });
+    };
+
+    if (status === STATUSES.SUCCESS) {
+        return (
+            <div className="p-4 rounded-md bg-green-50">
+                <div className="flex">
+                    <div className="flex-shrink-0">
+                        {/* Heroicon name: solid/check-circle */}
+                        <svg
+                            className="w-5 h-5 text-green-400"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            aria-hidden="true"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                                clipRule="evenodd"
+                            />
+                        </svg>
+                    </div>
+                    <div className="ml-3">
+                        <h3 className="text-sm font-medium text-green-800">Please check your email to confirm</h3>
+                        {/* <div className="mt-2 text-sm text-green-700">
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum
+                                similique veniam.
+                            </p>
+                        </div> */}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    if (status === STATUSES.ERROR) {
+        return (
+            <div className="p-4 rounded-md bg-red-50">
+                <div className="flex">
+                    <div className="flex-shrink-0">
+                        {/* Heroicon name: solid/x-circle */}
+                        <svg
+                            className="w-5 h-5 text-red-400"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            aria-hidden="true"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+                                clipRule="evenodd"
+                            />
+                        </svg>
+                    </div>
+                    <div className="ml-3">
+                        <h3 className="text-sm font-medium text-red-800">Something went wrong.</h3>
+                        <button
+                            className="mt-2 text-sm text-red-700 underline"
+                            onClick={event => {
+                                event.preventDefault();
+                                setStatus(STATUSES.INIT);
+                            }}
+                        >
+                            Click here to try again
+                        </button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
     return (
-        <form action="#" className="sm:max-w-xl sm:mx-auto lg:mx-0">
+        <form onSubmit={onSubmit} className="sm:max-w-xl sm:mx-auto lg:mx-0">
             <div className="sm:flex">
                 <div className="flex-1 min-w-0">
                     <label htmlFor="email" className="sr-only">
@@ -9,7 +112,9 @@ function SignUp({ label = 'Join wait list' }) {
                     <input
                         id="email"
                         type="email"
+                        name="email"
                         placeholder="Enter your email"
+                        disabled={status === STATUSES.SUBMITTING}
                         className="block w-full px-4 py-3 text-base text-gray-900 placeholder-gray-500 border rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-300 focus:ring-offset-gray-900"
                         data-cta="cta-input"
                     />
@@ -17,6 +122,7 @@ function SignUp({ label = 'Join wait list' }) {
                 <div className="mt-3 sm:mt-0 sm:ml-3">
                     <button
                         type="submit"
+                        disabled={status === STATUSES.SUBMITTING}
                         className="block w-full px-4 py-3 font-medium text-white bg-teal-500 shadow rounded-md hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-300 focus:ring-offset-gray-900"
                     >
                         {label}
