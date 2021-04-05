2989ccfe4dc6894243fb5f01956c98092170b0c8
Miklos Megyes <m.miklos05@gmail.com>
2021-04-05 18:33:05 +0200

    focus styling

--- react-app-actions.com/components/header.js
+++ react-app-actions.com/components/header.js
@@ -24,13 +24,13 @@ function Header() {
                 <div className="flex items-center justify-between py-6 md:justify-start md:space-x-10">
                     <div className="flex justify-start lg:w-0 lg:flex-1">
                         <Link href="/">
-                            <a className="block w-52 text-brand-green">
+                            <a className="block p-2 border-4 shadow w-52 text-brand-green border-brand-green">
                                 <span className="sr-only">Workflow</span>
                                 <Logo />
                             </a>
                         </Link>
                     </div>
-                    <div className="-my-2 -mr-2 md:hidden">
+                    {/* <div className="-my-2 -mr-2 md:hidden">
                         <button
                             type="button"
                             className="inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-teal-500"
@@ -38,32 +38,34 @@ function Header() {
                         >
                             <span className="sr-only">Open menu</span>
                         </button>
-                    </div>
+                    </div> */}
                     <nav className="hidden md:flex space-x-10">
                         <a
                             href="#vision"
                             onClick={smootScroll}
-                            className="text-base font-medium text-gray-500 hover:text-gray-900"
+                            className="text-xl font-medium text-gray-500 border-b-4 border-transparent hover:text-brand-green hover:border-brand-green"
                         >
                             Vision
                         </a>
                         <a
                             href="#how-does-it-work"
                             onClick={smootScroll}
-                            className="text-base font-medium text-gray-500 hover:text-gray-900"
+                            className="text-xl font-medium text-gray-500 border-b-4 border-transparent hover:text-brand-green hover:border-brand-green"
                         >
                             How does it work
                         </a>
                         <a
                             href="#benefits"
                             onClick={smootScroll}
-                            className="text-base font-medium text-gray-500 hover:text-gray-900"
+                            className="text-xl font-medium text-gray-500 border-b-4 border-transparent hover:text-brand-green hover:border-brand-green"
                         >
                             Benefits
                         </a>
-                        {/* <Link href="/about">
-                            <a className="text-base font-medium text-gray-500 hover:text-gray-900">About</a>
-                        </Link> */}
+                        <Link href="/about">
+                            <a className="text-xl font-medium text-gray-500 border-b-4 border-transparent hover:text-brand-green hover:border-brand-green">
+                                About
+                            </a>
+                        </Link>
                     </nav>
                     <div className="items-center justify-end hidden md:flex md:flex-1 lg:w-0">
                         <button
