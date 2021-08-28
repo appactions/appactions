7859b664fc69cb4ef0f7341d1510c1e7e9942103
Miklos Megyes <m.miklos05@gmail.com>
2021-08-29 01:38:00 +0200

    fix

--- cypress-version/cypress.js
+++ cypress-version/cypress.js
@@ -10,10 +10,8 @@ const cypress = require('cypress');
 (async () => {
     if (process.env.CI) {
         const result = await cypress.run({
-            config: {
-                record: !!process.env.CYPRESS_RECORD_KEY_FOR_APP_ACTIONS,
-                key: process.env.CYPRESS_RECORD_KEY_FOR_APP_ACTIONS,
-            },
+            record: !!process.env.CYPRESS_RECORD_KEY_FOR_APP_ACTIONS,
+            key: process.env.CYPRESS_RECORD_KEY_FOR_APP_ACTIONS,
         });
         if (result.runUrl) {
             console.log(`::set-output name=dashboardUrl::${result.runUrl}`);
