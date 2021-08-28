c4c0d672156bf14eedbb0d4303b17acef5897336
Miklos Megyes <m.miklos05@gmail.com>
2021-08-29 01:51:21 +0200

    debug

--- cypress-version/cypress.js
+++ cypress-version/cypress.js
@@ -13,6 +13,8 @@ const cypress = require('cypress');
             record: !!process.env.CYPRESS_RECORD_KEY_FOR_APP_ACTIONS,
             key: process.env.CYPRESS_RECORD_KEY_FOR_APP_ACTIONS,
         });
+        console.log('result keys', Object.keys(result))
+        console.log(`::set-output name=dashboardUrl::kekw.com`);
         if (result.runUrl) {
             console.log(`::set-output name=dashboardUrl::${result.runUrl}`);
         }
