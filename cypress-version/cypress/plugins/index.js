d9b0912381cb12232e407d9361e60aca3ca275e3
Miklos <m.miklos05@gmail.com>
2021-08-29 18:44:59 +0200

    unit tests in workflow (#13)

--- cypress-version/cypress/plugins/index.js
+++ cypress-version/cypress/plugins/index.js
@@ -4,13 +4,6 @@ const path = require('path');
 module.exports = on => {
     const options = webpack.defaultOptions;
 
-    // use the one in the source, not the copy in node_modules
-    options.webpackOptions.resolve = {
-        alias: {
-            'react-app-actions$': path.resolve('../react-app-actions'),
-        },
-    };
-
     on('file:preprocessor', webpack(options));
 
     on('before:browser:launch', (browser = {}, launchOptions) => {
--- cypress-version/webpack.config.js
+++ cypress-version/webpack.config.js
@@ -28,11 +28,6 @@ const config = {
                     },
                     {
                         loader: 'css-loader',
-                        options: {
-                            sourceMap: true,
-                            modules: true,
-                            localIdentName: '[local]___[hash:base64:5]',
-                        },
                     },
                 ],
             },
