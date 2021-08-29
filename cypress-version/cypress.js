d1b81e0abfdae6a56cc1e8fbd3de08b8ea94b1a6
Miklos Megyes <m.miklos05@gmail.com>
2021-08-29 02:07:41 +0200

    use check-runs

--- cypress-version/cypress.js
+++ cypress-version/cypress.js
@@ -14,6 +14,7 @@ const cypress = require('cypress');
             key: process.env.CYPRESS_RECORD_KEY_FOR_APP_ACTIONS,
         });
         console.log('result keys', Object.keys(result))
+        console.log('result runUrl', result.runUrl)
         console.log(`::set-output name=dashboardUrl::kekw.com`);
         if (result.runUrl) {
             console.log(`::set-output name=dashboardUrl::${result.runUrl}`);
