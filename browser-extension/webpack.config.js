41350c945ebd4471fe16fdac367e70e230a6678d
Miklos Megyes <m.miklos05@gmail.com>
2021-09-11 21:18:24 +0200

    add tailwind

--- browser-extension/webpack.config.js
+++ browser-extension/webpack.config.js
@@ -5,58 +5,67 @@ const ExtensionReloader = require('webpack-extension-reloader');
 const ManifestVersionSyncPlugin = require('webpack-manifest-version-sync-plugin');
 
 module.exports = {
-  entry: {
-    popup: ['core-js/stable', 'regenerator-runtime/runtime', './src/popup.js'],
-    content: ['core-js/stable', 'regenerator-runtime/runtime', './src/content.js'],
-    devtools: ['core-js/stable', 'regenerator-runtime/runtime', './src/devtools.js'],
-    background: ['core-js/stable', 'regenerator-runtime/runtime', './src/background.js'],
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'build'),
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.css'],
-    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-          },
+    entry: {
+        popup: ['core-js/stable', 'regenerator-runtime/runtime', './src/popup.js'],
+        content: ['core-js/stable', 'regenerator-runtime/runtime', './src/content.js'],
+        devtools: ['core-js/stable', 'regenerator-runtime/runtime', './src/devtools.js'],
+        background: ['core-js/stable', 'regenerator-runtime/runtime', './src/background.js'],
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'build'),
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css'],
+        modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.svg$/,
+                use: ['@svgr/webpack'],
+            },
         ],
-      },
-      {
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      },
+    },
+    plugins: [
+        new HTMLPlugin({
+            chunks: ['popup'],
+            filename: 'popup.html',
+        }),
+        new HTMLPlugin({
+            chunks: ['devtools'],
+            filename: 'devtools.html',
+        }),
+        new CopyPlugin([
+            { from: './src/assets', to: './assets' },
+            { from: './src/manifest.json', to: './manifest.json' },
+        ]),
+        new ExtensionReloader({
+            // manifest: path.resolve(__dirname, './src/manifest.json'),
+            entries: {
+              contentScript: ['content', 'devtools'],
+              background: 'background',
+              extensionPage: ['popup'],
+            }
+        }),
+        new ManifestVersionSyncPlugin(),
     ],
-  },
-  plugins: [
-    new HTMLPlugin({
-      chunks: ['popup'],
-      filename: 'popup.html',
-    }),
-    new HTMLPlugin({
-      chunks: ['devtools'],
-      filename: 'devtools.html',
-    }),
-    new CopyPlugin([
-      { from: './src/assets', to: './assets' },
-      { from: './src/manifest.json', to: './manifest.json' },
-    ]),
-    new ExtensionReloader({
-      manifest: path.resolve(__dirname, './src/manifest.json'),
-    }),
-    new ManifestVersionSyncPlugin(),
-  ],
-  optimization: {
-    minimize: true,
-  },
-  mode: 'production',
-  stats: 'minimal',
+    optimization: {
+        minimize: true,
+    },
+    mode: 'production',
+    stats: 'minimal',
 };
