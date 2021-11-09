d8edb612065bd39d9d86067e091c0c9c7b65b4fd
Miklos Megyes <m.miklos05@gmail.com>
2021-11-09 18:25:54 +0100

    wip

--- kitchensink-r3f/src/App.js
+++ kitchensink-r3f/src/App.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
 import { useSpring } from '@react-spring/core'
 import { a } from '@react-spring/web'
-import Scene from './Scene'
+import Scene from './TicTacToe'
 
 export default function App() {
   // This spring controls the background and the svg fill (text color)
@@ -12,7 +11,6 @@ export default function App() {
     <a.main style={{ background }}>
       <Canvas className="canvas" dpr={[1, 2]}>
         <Scene setBg={set} />
-        <OrbitControls enablePan={false} enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
       </Canvas>
     </a.main>
   )
