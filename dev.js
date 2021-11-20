5580190584a90188841435b6da44c304b4bf0ea6
Miklos Megyes <m.miklos05@gmail.com>
2021-11-20 19:24:25 +0100

    improve DX

--- dev.js
+++ dev.js
@@ -1,21 +1,58 @@
 const inquirer = require('inquirer');
+const concurrently = require('concurrently');
+
+process.on('unhandledRejection', err => {
+    throw err;
+});
 
 const choices = [
-    { label: 'Cypress version with Nextjs kitchensink', value: 'dev:cypress:nextjs' },
-    { label: 'Cypress version with R3F kitchensink', value: 'dev:cypress:r3f' },
-    { label: 'Standalone App Actions', value: 'dev:app-actions' },
+    {
+        label: 'Cypress version with Nextjs kitchensink',
+        concurrently: [
+            { name: 'cyappactions', command: 'yarn workspace cypress-app-actions build --watch', prefixColor: 'blue' },
+            { name: 'browser-ext', command: 'yarn workspace browser-extension dev', prefixColor: 'yellow' },
+            { name: 'kitchensink', command: 'yarn workspace kitchensink-nextjs cypress', prefixColor: 'green' },
+        ],
+    },
+    {
+        label: 'Cypress version with R3F kitchensink',
+        concurrently: [
+            { name: 'cyappactions', command: 'yarn workspace cypress-app-actions build --watch', prefixColor: 'blue' },
+            { name: 'browser-ext', command: 'yarn workspace browser-extension dev', prefixColor: 'yellow' },
+            { name: 'kitchensink', command: 'yarn workspace kitchensink-r3f cypress', prefixColor: 'green' },
+        ],
+    },
+    {
+        label: 'Standalone React App Actions',
+        concurrently: [
+            // TODO: improve this once react-app-actions is developed again
+            {
+                name: 'reactappactions',
+                command: 'yarn workspace react-app-actions rollup --config rollup.config.js --watch',
+                prefixColor: 'blue',
+            },
+        ],
+    },
 ];
 
-inquirer
-    .prompt([
-        {
-            type: 'list',
-            name: 'project',
-            message: 'Which project do you want to develop?',
-            choices: choices.map(x => x.label),
-        },
-    ])
-    .then(answers => {
-        const project = choices.find(x => x.label === answers.project).value;
-        console.log({ project });
+const prompt = inquirer.createPromptModule({ output: process.stderr });
+
+prompt([
+    {
+        type: 'list',
+        name: 'project',
+        message: 'Which project do you want to develop?',
+        choices: choices.map(x => x.label),
+    },
+]).then(answers => {
+    const commands = choices.find(x => x.label === answers.project).concurrently;
+
+    concurrently(commands, {
+        prefix: 'name',
+        killOthers: ['failure', 'success'],
     });
+});
+
+// "dev:cypress:nextjs": "concurrently -n 'cyappactions,browser-ext,kitchensink' -c 'blue,yellow,green' 'yarn workspace cypress-app-actions build --watch' 'yarn workspace browser-extension dev' 'yarn workspace kitchensink-nextjs cypress' --kill-others",
+// "dev:cypress:r3f": "concurrently -n 'cyappactions,browser-ext,kitchensink' -c 'blue,yellow,green' 'yarn workspace cypress-app-actions build --watch' 'yarn workspace browser-extension dev' 'yarn workspace kitchensink-r3f cypress' --kill-others",
+// "dev:cypress-app-actions": "yarn workspace cypress-app-actions build:react-devtools-renderer && yarn workspace cypress-app-actions build && yarn dev:cypress",
