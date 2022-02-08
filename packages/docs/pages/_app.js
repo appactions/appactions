d2e5fdf787b8ca2b6b155a0bc873fb1852945adf
Miklos Megyes <m.miklos05@gmail.com>
2022-02-08 21:39:52 +0100

    add tailwind

--- packages/docs/pages/_app.js
+++ packages/docs/pages/_app.js
@@ -1,19 +1,5 @@
-import Logo from 'components/logo';
-import Link from 'next/link';
 import '../styles/global.css';
 
 export default function App({ Component, pageProps }) {
-    return (
-        <>
-            <Link href="/">
-                <a className="block p-2 w-52 text-white">
-                    <span className="inline-block w-9 h-6">
-                        <Logo />
-                    </span>
-                    App Actions
-                </a>
-            </Link>
-            <Component {...pageProps} />
-        </>
-    );
+    return <Component {...pageProps} />;
 }
