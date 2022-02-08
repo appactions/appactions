80bbd72977240f9bdb29f8f527bc7d5452b5ceaf
Miklos Megyes <m.miklos05@gmail.com>
2022-02-08 21:28:38 +0100

    docs

--- packages/docs/pages/_app.js
+++ packages/docs/pages/_app.js
@@ -1,5 +1,19 @@
-import '../styles/global.css'
+import Logo from 'components/logo';
+import Link from 'next/link';
+import '../styles/global.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Link href="/">
+                <a className="block p-2 w-52 text-white">
+                    <span className="inline-block w-9 h-6">
+                        <Logo />
+                    </span>
+                    App Actions
+                </a>
+            </Link>
+            <Component {...pageProps} />
+        </>
+    );
 }
