d542b7304a2ba081cdc675cba99bd3510b873d0e
Miklos Megyes <m.miklos05@gmail.com>
2022-03-02 21:13:11 +0100

    add react trello

--- packages/kitchensink-trello/pages/_app.jsx
+++ packages/kitchensink-trello/pages/_app.jsx
@@ -1,7 +1,16 @@
+import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <div className="min-h-screen">
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Component {...pageProps} />
+        </div>
+    );
 }
 
 export default MyApp;
