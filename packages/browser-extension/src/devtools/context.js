715162fd63e9b6adda5e57a41faa6b4dedf379c1
Miklos Megyes <m.miklos05@gmail.com>
2022-03-16 19:44:05 +0100

    delete browser-extension package

--- packages/browser-extension/src/devtools/context.js
+++
@@ -1,5 +0,0 @@
-import { createContext, useContext } from 'react';
-
-export const DevtoolsContext = createContext(null);
-
-export const useDevtoolsContext = () => useContext(DevtoolsContext);
--- packages/browser-extension/src/panel.js
+++
@@ -1,5 +0,0 @@
-import './style.css';
-
-const el = document.createElement('div');
-document.body.appendChild(el);
-window.container = el;
