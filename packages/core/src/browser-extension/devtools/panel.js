f2140ea8c58114de328b4e590397000865bb1cc7
Miklos Megyes <m.miklos05@gmail.com>
2022-04-26 18:10:14 +0200

    fix tab padding

--- packages/core/src/browser-extension/devtools/panel.js
+++ packages/core/src/browser-extension/devtools/panel.js
@@ -41,7 +41,7 @@ const Panel = () => {
                                         tab.selected
                                             ? 'border-indigo-500 text-indigo-600'
                                             : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
-                                        'whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm cursor-pointer',
+                                        'whitespace-nowrap py-3 px-1 border-b-2 font-medium text-sm cursor-pointer',
                                     )}
                                     onClick={event => {
                                         event.preventDefault();
