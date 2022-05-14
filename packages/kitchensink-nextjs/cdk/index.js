83f66cffad006ef763984f41cb9f5752f5dd5cad
Miklos Megyes <m.miklos05@gmail.com>
2022-05-14 22:10:40 +0200

    delete folder

--- packages/kitchensink-nextjs/cdk/index.js
+++
@@ -1,7 +0,0 @@
-const { App } = require('@aws-cdk/core');
-const { DBStack } = require('./db-stack');
-
-const app = new App();
-
-const stackName = `RestaurantReviewer-DBStack-${process.env.STAGE}`;
-new DBStack(app, stackName);
--- packages/kitchensink-nextjs/components/main.js
+++
@@ -1,5 +0,0 @@
-function Main({ children }) {
-    return <main className="container px-4 mx-auto my-8 sm:px-6 lg:px-8">{children}</main>;
-}
-
-export default Main;
