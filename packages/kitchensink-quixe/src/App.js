25ec7c629a952d6f2af8c365619a3b78f0f55754
Miklos Megyes <m.miklos05@gmail.com>
2022-06-29 14:48:44 +0200

    init MutationObserver

--- packages/kitchensink-quixe/src/App.js
+++ packages/kitchensink-quixe/src/App.js
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+const callback = mutationList => {
+    console.log('-------')
+    for (const mutation of mutationList) {
+        if (mutation.addedNodes.length > 0) {
+            console.log('mutation', mutation);
+        }
+    }
+};
+
+const config = {
+    childList: true,
+    subtree: true,
+};
+
+const targetNode = document.getElementById('gameport');
 
 export default function App() {
-    return <h1>Hello</h1>;
+    useEffect(() => {
+        const observer = new MutationObserver(callback);
+        observer.observe(targetNode, config);
+        return () => observer.disconnect();
+    }, []);
+    return <h1>YOLO</h1>;
 }
