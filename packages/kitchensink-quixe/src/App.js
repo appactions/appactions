09ead86b9c7929a71d755069d778bf17ca7dc5cb
Miklos Megyes <m.miklos05@gmail.com>
2022-06-29 20:02:44 +0200

    scrape text

--- packages/kitchensink-quixe/src/App.js
+++ packages/kitchensink-quixe/src/App.js
@@ -1,12 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useReducer } from 'react';
 
-const callback = mutationList => {
-    console.log('-------')
+const filteredText = new Set(['', '>']);
+
+const callback = dispatch => mutationList => {
+    // dispatch({ type: 'startScene' });
     for (const mutation of mutationList) {
         if (mutation.addedNodes.length > 0) {
-            console.log('mutation', mutation);
+            for (const node of mutation.addedNodes) {
+                const text = node.textContent.trim();
+                if (filteredText.has(text)) {
+                    continue;
+                }
+
+                if (node.classList.contains('BufferLine')) {
+                    dispatch({ type: 'addText', text });
+                }
+            }
         }
     }
+    // dispatch({ type: 'endScene' });
 };
 
 const config = {
@@ -16,11 +28,40 @@ const config = {
 
 const targetNode = document.getElementById('gameport');
 
-export default function App() {
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'startScene': {
+            return state;
+        }
+        case 'addText': {
+            return {
+                ...state,
+                text: action.text,
+            };
+        }
+        case 'endScene': {
+            return state;
+        }
+        default: {
+            return state;
+        }
+    }
+};
+
+const useBridge = () => {
+    const [state, dispatch] = useReducer(reducer, { text: null });
+
     useEffect(() => {
-        const observer = new MutationObserver(callback);
+        const observer = new MutationObserver(callback(dispatch));
         observer.observe(targetNode, config);
         return () => observer.disconnect();
     }, []);
+
+    return state;
+};
+
+export default function App() {
+    const state = useBridge();
+    console.log('state', state);
     return <h1>YOLO</h1>;
 }
