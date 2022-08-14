41e7fff2c124fe5436adf5d30c4043867ee8cd54
Miklos Megyes <m.miklos05@gmail.com>
2022-08-15 00:51:30 +0200

    cypress viewport

--- packages/kitchensink-quixe/cypress.config.js
+++ packages/kitchensink-quixe/cypress.config.js
@@ -3,6 +3,7 @@ const { addPlugin } = require('@appactions/core/plugin');
 const preprocessor = require('@appactions/core/preprocessor');
 
 module.exports = defineConfig({
+    viewportHeight: 1500,
     e2e: {
         specPattern: 'flows/**/*.{yml,yaml}',
         baseUrl: 'http://localhost:3000',
